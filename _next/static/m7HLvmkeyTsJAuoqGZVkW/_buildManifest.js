8c4aec9f68d7e04ce9015846deab9884a8f3db34
Rahul R <hybrid@hybrids-MacBook-Pro.local>
2026-08-28 23:28:14 +0530

    Updates

---
+++ _next/static/m7HLvmkeyTsJAuoqGZVkW/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/decentralized-healthcare//_next/:path+",
+        "destination": "/decentralized-healthcare/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
